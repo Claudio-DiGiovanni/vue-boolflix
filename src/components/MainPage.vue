62a12730a2d8dea0b9ad13c94d3c49c692e69487
Claudio-DiGiovanni <digioclaudio97@gmail.com>
2022-11-30 15:53:01 +0100

    milestone 2

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -4,11 +4,19 @@
     <label for="t">
       <input type="text" name="t" id="t" v-model="searchString" @keyup.enter="searchMovie">
     </label>
+    <h1>film</h1>
     <ul v-for="movie in arrMovies" :key="movie.id">
       <li>{{ movie.title }}</li>
       <li>{{ movie.original_title }}</li>
       <li>{{ movie.vote_average }}</li>
       <li><span :class="('fi fi-' + (movie.original_language != 'en' ? movie.original_language : 'gb'))"></span></li>
+    </ul>
+    <h1>serie tv</h1>
+    <ul v-for="TVShow in arrTVShow" :key="TVShow.id">
+      <li>{{ TVShow.name }}</li>
+      <li>{{ TVShow.original_name }}</li>
+      <li>{{ TVShow.vote_average }}</li>
+      <li><span :class="('fi fi-' + (TVShow.original_language != 'en' ? TVShow.original_language : 'gb'))"></span></li>
     </ul>
   </div>
 </template>
@@ -21,6 +29,7 @@ export default {
     return {
       searchString: '',
       arrMovies: null,
+      arrTVShow: null,
     };
   },
   methods: {
@@ -28,6 +37,10 @@ export default {
       axios.get(`https://api.themoviedb.org/3/search/movie?query=${this.searchString}&api_key=4886c22c895eeca818d2be897ecf2417`)
         .then((axiosResponse) => {
           this.arrMovies = axiosResponse.data.results;
+        });
+      axios.get(`https://api.themoviedb.org/3/search/tv?query=${this.searchString}&api_key=4886c22c895eeca818d2be897ecf2417`)
+        .then((axiosResponse) => {
+          this.arrTVShow = axiosResponse.data.results;
         });
     },
   },
